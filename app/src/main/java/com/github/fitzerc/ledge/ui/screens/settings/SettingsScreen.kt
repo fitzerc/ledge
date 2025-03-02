f033a6467733444d50221308beff316566fddcf9
chris <fitzer.c@gmail.com>
2025-03-02 12:31:17 -0600

    enhancing ux on settings screen

--- app/src/main/java/com/github/fitzerc/ledge/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/github/fitzerc/ledge/ui/screens/settings/SettingsScreen.kt
@@ -2,9 +2,17 @@ package com.github.fitzerc.ledge.ui.screens.settings
 
 import android.content.Context
 import android.os.Environment
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -25,6 +35,11 @@ import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
 
+class SettingOption(
+    val text: String,
+    val onClick: () -> Unit
+)
+
 @Composable
 fun SettingsScreen(navigationController: NavController, ledgeDb: LedgeDatabase) {
     val context = LocalContext.current
@@ -32,16 +47,40 @@ fun SettingsScreen(navigationController: NavController, ledgeDb: LedgeDatabase) 
 
     var showAuthorDialog by remember { mutableStateOf(false) }
 
-    Scaffold(modifier = Modifier.padding(horizontal = 16.dp)) { paddingInner ->
+    val settingsOptions = listOf(
+        SettingOption("Manage Authors", onClick = { showAuthorDialog = true }),
+        SettingOption("Manage Series", onClick = { navigationController.navigate("manageseries") }),
+        SettingOption("Backup Data", onClick = { backupData(ledgeDb.dbVersion, context, coroutineScope) })
+    )
+
+    //TODO: may need bottomPadding passed in
+    Scaffold(modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)) { paddingInner ->
         Column(modifier = Modifier.padding(paddingInner)) {
-            Button(onClick = { showAuthorDialog = true }) {
-                Text(text = "Manage Authors")
-            }
-            Button(onClick = { navigationController.navigate("manageseries") }) {
-                Text(text = "Manage Series")
-            }
-            Button(onClick = { backupData(ledgeDb.dbVersion, context, coroutineScope) }) {
-                Text(text = "Backup Data")
+            Text(text = "Settings", style = MaterialTheme.typography.headlineMedium)
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                items(settingsOptions) { opt ->
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = { opt.onClick() }
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Text(
+                                style = MaterialTheme.typography.titleLarge,
+                                text = opt.text,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp)
+                            )
+                        }
+                    }
+                }
             }
         }
     }
